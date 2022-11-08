64f44823c2b06112193d0f821d59ad375eea18e2
JoannaGoj <joannagojan@gmail.com>
2022-11-08 09:40:53 +0100

    Added task instructions

--- src/Cards.java
+++ src/Cards.java
@@ -1,3 +1,31 @@
+//Instructions for Assignment 4
+// Invoking Math.random() returns a (pseudo)random number of type double from
+//the half-open interval [0, 1). Use this generator to draw cards from a standard deck.
+//First, draw an integer from the interval [1, 4] (corresponding to suit: clubs, diamonds,
+//hearts, spades) and then another number from the interval [2, 14] (deuce, trey, four,
+//five, . . . , ten, Jack, Queen, King, Ace). Using the switch expression, define strings
+//describing cards and display five such random cards on the console. For example, the
+//following program
+//download Cards.java
+//public class Cards {
+//public static void main(String[] args) {
+//for (int i = 0; i < 5; ++i) {
+///*
+//* Define two integers using only Math.random():
+//*
+//col is a random integer from range [1, 4]
+//*
+//rnk is a random integer from range [2, 14]
+//*/
+//String color = switch(col) {
+//// ...
+//};
+//String rank = switch(rnk) {
+//// ...
+//};
+//System.out.println(rank + " of " + color);
+
+
 
 public class Cards {
     public static void main(String[] args) {
